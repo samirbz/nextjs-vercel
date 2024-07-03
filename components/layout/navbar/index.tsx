76bce93ac20eda6cb7da3d708d5b2be31ff11b1e
samir <febsec14@gmail.com>
2024-07-03 14:18:55 +0545

    navbar working....

---
+++ components/layout/navbar/index.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import MobileMenu from './mobile-menu';
+import LogoSquare from '@/components/logo-square';
+const { SITE_NAME } = process.env;
+
+export default async function Navbar() {
+  return (
+    <nav className="relative flex items-center justify-between p-4 lg:px-6">
+      <div className="block flex-none md:hidden">
+        <MobileMenu />
+      </div>
+      <div className="flex w-full items-center">
+        <div className="flex w-full md:w-1/3">
+          <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
+            <LogoSquare />
+            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
+              {SITE_NAME}
+            </div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ components/layout/navbar/mobile-menu.tsx
@@ -0,0 +1,10 @@
+export default function MobileMenu() {
+  return (
+    <>
+      <button
+        aria-label="Open mobile menu"
+        className="flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors md:hidden dark:border-neutral-700 dark:text-white"
+      ></button>
+    </>
+  );
+}
